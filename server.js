859e22c644114876fa91d431f5d9bb46759d3cea
chaya roth <chayarero@gmail.com>
2017-10-01 16:27:56 +0300

    start

---
+++ server.js
@@ -0,0 +1,14 @@
+/**
+ * Created by chaya on 10/1/2017.
+ */
+var chaya = function () {
+    return {
+        Name:function () {
+            return 'chaya';
+        },
+        Family:function () {
+            return 'roth';
+        }
+    }
+};
+module.exports = chaya;
